ba84f0c20addaa76b5ca74100e1640415e4b0516
STUDENT Kacper Klocek <Kacper.Klocek@students.ittralee.ie>
2022-09-29 10:49:00 +0100

    Continuing with the movement of our character Signed-off-by: STUDENT Kacper Klocek <Kacper.Klocek@students.ittralee.ie>

--- TheCubeGame/Assets/Cubecontrol.cs
+++ TheCubeGame/Assets/Cubecontrol.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Cubecontrol : MonoBehaviour
 {
+    private const string V = "w";
+    private float turningspeed = 180;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,7 +17,28 @@ public class Cubecontrol : MonoBehaviour
     void Update()
     {
         if (Input.GetKey(KeyCode.UpArrow))
-            
-        transform.position += Vector3.up* Time.deltaTime;  
+          
+        transform.position += Vector3.up* Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow))
+
+            transform.position += Vector3.down* Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+
+            transform.position += Vector3.left* Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow))
+
+            transform.position += Vector3.right * Time.deltaTime;
+        if (true)
+        {
+            if (Input.GetKey(KeyCode.A))
+
+                transform.Rotate(Vector3.up,-turningspeed* Time.deltaTime);
+
+
+
+            if (Input.GetKey(KeyCode.D))
+
+                transform.Rotate(Vector3.up, turningspeed * Time.deltaTime);
+        }
     }
 }
